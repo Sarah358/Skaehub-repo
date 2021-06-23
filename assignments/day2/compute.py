0429a0786442fbbf6b987788cc91b4a9d874191d
sarah <njerinjuguna358@gmail.com>
2021-06-23 22:28:47 +0300

    perform simple statistics on values

---
+++ assignments/day2/compute.py
@@ -0,0 +1,12 @@
+# import stats module
+from statistics import mean, median, mode, stdev
+# list of values
+scores = [20,30,60,74,58,21,56,85,45]
+
+# print mean scores
+print(mean(scores))
+# print median
+print(median(scores))
+# print mode
+print(mode(scores))
+
