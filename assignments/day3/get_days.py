574171780a8995b03302b02a601a18eebd2be38b
sarah <njerinjuguna358@gmail.com>
2021-06-25 08:48:07 +0300

    Get number of weekdays

--- assignments/day3/get_days.py
+++ assignments/day3/get_days.py
@@ -1,4 +1,13 @@
 import numpy as np
-print("Number of weekdays in Jan 2020:")
-print(np.busday_count('2020-01'))
+print('Enter From Day YYYY-MM e.g 2021-07')
+# allow user to input the start date 
+start_count = input()
+print('Enter To Day YYYY-MM e.g 2021-07')
+# allow user to input stop day
+stop_count = input()
+# count number of weekdays from startcount to stopcount
+s= np.busday_count(start_count, stop_count)
+# print number of weekdays 
+print('Number of weekdays : ')
+print(s)
 
